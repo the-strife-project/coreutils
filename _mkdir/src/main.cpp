016f10f2e74c14ae7851ca77477e8b2b7612b642
jlxip <jlxip@protonmail.com>
2022-07-21 02:19:34 +0200

    mkdir

--- _mkdir/src/main.cpp
+++ _mkdir/src/main.cpp
@@ -1,7 +1,26 @@
 #include <cstdio>
 #include <syscalls>
+#include <tasks>
+#include <fs>
 
-extern "C" void _start() {
-	std::printf("Hello world\n");
-	std::exit();
+extern "C" void _start(std::Runtime rt) {
+	auto rtv = rt.parse();
+
+	if(!rtv.args.size()) {
+		std::printf("mkdir: missing operand\n");
+		std::exit(1);
+	}
+
+	std::string& path = rtv.args[0];
+	if(path[0] != '/') {
+		if(!rtv.env.has("PWD")) {
+			std::printf("mkdir: no $PWD set\n");
+			std::exit(2);
+		}
+
+		path = rtv.env["PWD"] + "/" + path;
+	}
+	path = std::simplifyPath(path);
+
+	std::exit(std::mkdir(path));
 }
